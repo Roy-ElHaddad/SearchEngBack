acd78cab181b1b097ecb504fb5b33dfaa5db2269
Roy-ElHaddad <royhaddad27@gmail.com>
2022-12-30 00:19:49 +0200

    version 1

---
+++ configure.js
@@ -0,0 +1,3 @@
+export default {
+    API_KEY:'32221629-391c4d728accc9bac59ec0159'
+}
---
+++ server.js
@@ -0,0 +1,39 @@
+import config from "./configure.js";
+import express  from "express"
+import cors from "cors"
+// import {imageObject} from "./types"
+const app = express()
+let param = ''
+
+app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.post("/store-data", (req , res) =>{
+    param = req.body.searchvalue
+    console.log(param)
+    res.send("works")
+})
+
+app.get("/images", async (req, res) =>{
+    let URL = "https://pixabay.com/api/?key="+config.API_KEY+"&q="+encodeURIComponent(param)
+    let str = [] //:string[] = []
+    console.log(URL)
+    const response = await fetch(URL)
+    const data  = await response.json()
+    let content = data.hits //:imageObject[] = data.hits 
+    if (content && content.length > 0)
+    {
+        content.map((item, index) => {
+            str[index] = item.webformatURL
+        })
+        res.send(str)
+    }    
+})
+
+app.listen(5000)
