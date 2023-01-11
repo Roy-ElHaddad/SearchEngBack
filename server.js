9ff3e9c814c2b6cbac3b09a07368ffd6b2c5177d
Roy-ElHaddad <royhaddad27@gmail.com>
2023-01-11 09:44:57 +0200

    v2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import config from "./configure.js";
 import express  from "express"
 import cors from "cors"
-// import {imageObject} from "./types"
+
 const app = express()
 let param = ''
 
@@ -14,6 +14,7 @@ app.use(
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
 app.post("/store-data", (req , res) =>{
     param = req.body.searchvalue
     console.log(param)
@@ -22,11 +23,11 @@ app.post("/store-data", (req , res) =>{
 
 app.get("/images", async (req, res) =>{
     let URL = "https://pixabay.com/api/?key="+config.API_KEY+"&q="+encodeURIComponent(param)
-    let str = [] //:string[] = []
+    let str = [] 
     console.log(URL)
     const response = await fetch(URL)
     const data  = await response.json()
-    let content = data.hits //:imageObject[] = data.hits 
+    let content = data.hits 
     if (content && content.length > 0)
     {
         content.map((item, index) => {
